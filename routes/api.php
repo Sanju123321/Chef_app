3f897e024f53fe18bd1afd09f59493eca71d6a27
Ubuntu <ubuntu@ip-172-31-44-237.us-east-2.compute.internal>
2021-07-20 17:37:56 +0000

    chages of live

--- routes/api.php
+++ routes/api.php
@@ -47,6 +47,9 @@ Route::group(['prefix'=>'chef'],function(){
 
 
 
+Route::post('/get_list', [ChefController::class, 'get_list']);
+
+
 Route::get('/country-list', [CommonController::class, 'country_list']);
 Route::post('/chef/listing', [CommonController::class, 'chef_listing']);
 Route::post('/dish/listing', [CommonController::class, 'dish_listing']);
